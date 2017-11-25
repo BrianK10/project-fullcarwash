bba9284205931ecb2758a7723c1fba9814ce3010
BrianK10 <bkahn.james@gmail.com>
2017-11-25 17:40:50 -0500

    Connect to GitHub

---
+++ wcf_fullcarwash/ICustomers.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wcf_fullcarwash
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IServiceCustomer
+    {
+        [OperationContract]
+        List<customers> getcustomers();
+
+        [OperationContract]
+        Boolean insertcustomer(customers objcustomer);
+
+        [OperationContract]
+        Boolean updatecustomer(customers objcustomer);
+    }
+
+    [DataContract]
+    [Serializable]
+    public class customers
+    {
+        private int _id;
+
+        [DataMember]
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        private string _firstname;
+
+        [DataMember]
+        public string firstname
+        {
+            get { return _firstname; }
+            set { _firstname = value; }
+        }
+        private string _lastname;
+
+        [DataMember]
+        public string lastname
+        {
+            get { return _lastname; }
+            set { _lastname = value; }
+        }
+        private string _fullname;
+
+        [DataMember]
+        public string fullname
+        {
+            get { return _fullname; }
+            set { _fullname = value; }
+        }
+        private bool _gender;
+
+        [DataMember]
+        public bool gender
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+        private DateTime _birthdate;
+
+        [DataMember]
+        public DateTime birthdate
+        {
+            get { return _birthdate; }
+            set { _birthdate = value; }
+        }
+        private string _phone;
+
+        [DataMember]
+        public string phone
+        {
+            get { return _phone; }
+            set { _phone = value; }
+        }
+        private string _email;
+
+        [DataMember]
+        public string email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+        private string _numberdni;
+
+        [DataMember]
+        public string numberdni
+        {
+            get { return _numberdni; }
+            set { _numberdni = value; }
+        }
+        private string _address;
+
+        [DataMember]
+        public string address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+        private string _password;
+
+        [DataMember]
+        public string password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+    }
+}
---
+++ wcf_fullcarwash/ServiceCustomers.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Data.Entity.Core;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wcf_fullcarwash
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código y en el archivo de configuración a la vez.
+    public class ServiceCustomers : IServiceCustomer
+    {
+        Boolean value;
+        public List<customers> getcustomers()
+        {
+            fullcarwashEntities model = new fullcarwashEntities();
+            List<customers> objlstcustomer = new List<customers>();
+
+            try
+            {
+                var query = model.SP_SELECT_CUSTOMERS();
+                foreach (var result in query)
+                {
+                    customers objcustomer = new customers();
+
+                    objcustomer.id = result.idCustomer;
+                    objcustomer.firstname= result.firstName;
+                    objcustomer.lastname = result.lastName;
+                    objcustomer.fullname = result.fullName;
+                    objcustomer.gender = result.gender;
+                    objcustomer.birthdate = result.birthdate;
+                    objcustomer.phone = result.phone;
+                    objcustomer.email = result.email;
+                    objcustomer.numberdni = result.number_dni;
+                    objcustomer.address = result.address;
+                    objcustomer.password = result.password;
+
+                    objlstcustomer.Add(objcustomer);
+                }
+            }
+            catch (EntityException exception)
+            {
+                throw new Exception(exception.Message);
+
+            }
+            return objlstcustomer;
+        }
+
+        public Boolean insertcustomer(customers objcustomer)
+        {
+            fullcarwashEntities model = new fullcarwashEntities();
+            try
+            {
+                Customers objcust = new Customers();
+
+                objcust.firstName = objcustomer.firstname;
+                objcust.lastName = objcustomer.lastname;
+                objcust.fullName = objcustomer.firstname + " " + objcustomer.lastname;
+                objcust.gender = objcustomer.gender;
+                objcust.birthdate = objcustomer.birthdate;
+                objcust.phone = objcustomer.phone;
+                objcust.email = objcustomer.email;
+                objcust.number_dni = objcustomer.numberdni;
+                objcust.address = objcustomer.address;
+                objcust.password = objcustomer.password;
+
+
+                model.Customers.Add(objcust);
+                model.SaveChanges();
+
+                value = true;
+            }
+            catch (EntityException ex)
+            {
+                ex.Message.ToString();
+                value = false;
+            }
+            return value;
+        }
+
+        public Boolean updatecustomer(customers objcustomer)
+        {
+            fullcarwashEntities model = new fullcarwashEntities();
+            try
+            {
+                Customers objcust = (from objc in model.Customers
+                                     where objc.idCustomer == objcustomer.id
+                                     select objc).FirstOrDefault();
+
+                objcust.firstName = objcustomer.firstname;
+                objcust.lastName = objcustomer.lastname;
+                objcust.fullName = objcustomer.firstname + " " + objcustomer.lastname;
+                objcust.gender = objcustomer.gender;
+                objcust.birthdate = objcustomer.birthdate;
+                objcust.phone = objcustomer.phone;
+                objcust.email = objcustomer.email;
+                objcust.number_dni = objcustomer.numberdni;
+                objcust.address = objcustomer.address;
+                objcust.password = objcustomer.password;
+                
+                model.SaveChanges();
+                value = true;
+            }
+            catch (EntityException ex)
+            {
+                ex.Message.ToString();
+                value = false;
+            }
+            return value;           
+        }        
+    }
+}
